13cdf1551e5a395e5f7cac0d45da5e502e130373
H.L <lh151732@gmail.com>
2024-10-21 14:53:28 +1100

    no changes made

--- src/main/java/com/elec5620/readverseserver/ReadVerseServerApplication.java
+++ src/main/java/com/elec5620/readverseserver/ReadVerseServerApplication.java
@@ -1,8 +1,9 @@
 package com.elec5620.readverseserver;
 
-import com.elec5620.readverseserver.utils.FileHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.elec5620.readverseserver.utils.FileHandler;
 
 @SpringBootApplication
 public class ReadVerseServerApplication {
