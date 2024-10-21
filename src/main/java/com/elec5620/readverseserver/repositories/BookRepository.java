7915a2cc27309cb81f9a3f96dab5dff45179efbe
H.L <lh151732@gmail.com>
2024-10-21 15:16:33 +1100

    delete search summary in searchBook query

--- src/main/java/com/elec5620/readverseserver/repositories/BookRepository.java
+++ src/main/java/com/elec5620/readverseserver/repositories/BookRepository.java
@@ -1,20 +1,18 @@
 package com.elec5620.readverseserver.repositories;
 
-import com.elec5620.readverseserver.models.Book;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.elec5620.readverseserver.models.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findBooksByPublisherId(Long publisherId);
     @Query("SELECT b FROM Book b WHERE b.publisherId = :publisherId AND " +
         "(b.title ILIKE CONCAT('%', :keyword, '%') OR " +
-        "b.author ILIKE CONCAT('%', :keyword, '%') OR " +
-        "b.summary ILIKE CONCAT('%', :keyword, '%'))")
+        "b.author ILIKE CONCAT('%', :keyword, '%')")
     List<Book> findBooksByPublisherIdAndKeyword(Long publisherId, String keyword);
 }
